1d2ce08d8df22bb803138d247035892852276ad8
om raman <omraman@gmail.com>
2019-09-06 17:24:51 +0530

    new

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Samrat Investment</title>
+
+ <?php include "link/header.html" ?> 
+<section id="Home-page">  
+<div class="container-fluid">
+  <nav class="navbar navbar-expand-sm navbar-light bg-light header justify-content-center pt-5 pb-5">
+    <ul class="navbar-nav">
+      <li class="nav-item ml-5">
+        <div align="center">
+          <img src="image/per.png" class="img-fluid" style="height: 35px; width: 40px;">
+        </div>
+        <a class="nav-link font" href="#">Personal Saving</a>
+      </li>
+      <li class="nav-item ml-5">
+        <div align="center">
+          <img src="image/business.png" class="img-fluid" style="height: 35px; width: 40px;">
+        </div>
+        <a class="nav-link font" href="#">Financial Consultancy</a>
+      </li>
+      <li class="nav-item ml-5">
+        <div align="center">
+          <img src="image/fund.png" class="img-fluid" style="height: 35px; width: 40px;">
+        </div>
+        <a class="nav-link font" href="#">Finding</a>
+      </li>
+      <li class="nav-item ml-5">
+        <div align="center">
+          <img src="image/freedom.png" class="img-fluid" style="height: 35px; width: 40px;">
+        </div>
+        <a class="nav-link font" href="#">Freedom Venture</a>
+      </li>
+      <li class="nav-item ml-5"> 
+        <div align="center">
+          <img src="image/inno.png" class="img-fluid" style="height: 35px; width: 40px;">
+        </div><a class="nav-link font" href="#">Innovation</a>
+      </li>
+      <li class="nav-item ml-5">
+        <div align="center">
+          <img src="image/sus2.png" class="img-fluid" style="height: 35px; width: 40px;">
+          </div>
+          <a class="nav-link font" href="#">Sustainability</a>
+        </li>
+      </ul>
+    </nav>
+<section class="d-none d-sm-block">
+ <div class="row">
+
+   <div class="col-sm-7 colore">
+     <div align="center"> 
+       <h1 class="world">World we define together</h1>
+       
+       <div class="center" class="example">
+          <div class="row">
+            <p class="font2 margin">We <b class="fontl"> envolved to transform </b> the</p>
+          </div>
+          <div class="row">
+            <p class="font2 margin">way your economy works for <b class="fontl"> you, </b> to a <b class="fontl"> great</b></p>
+          </div>
+          <div class="row">
+            <p class="font2 margin"><b class="fontl">experience. </b> What you have earned;</p>
+          </div>
+          <div class="row">
+            <p class="font2 margin">serving you the <b class="fontl">Value & Profit. </b></p>
+          </div>
+        </div>
+       
+     </div>
+   </div><br><br>
+
+   <div class="col-sm-5">
+     <div class="container-fluid">
+        <img src="image/mobile.png" class="img-fluid w-100">
+        <div class="text-block" >
+          <div class="row" style="padding-right: 10px; padding-left: 20px;">
+            <div class="col-sm-12">
+              <h5 class="row mb-0" style="color: #F02B08; margin-left: 0.3px;"><b>Samrat<h5 style="color: blue;" class="ml-1"> Investment</b></h5></h5>
+              <h6 class="mt-0 ml-0" style="font-size: 14px;"><b>No more a banker, somthing beyond</b></h6>
+            </div>
+            <div class="col-sm-12">
+              <h6 style="font-size: 20px; color: blue;"><img src="image/char.png" class="img-fluid" style="height: 25px; width: 30px; color: blue;"><a href="#Financial"><b>Financial Service</b></a><p style="font-size: 15px; margin-left: 30px; color: black;">reducing risk to saving profitability</p></h6>
+            </div>
+            <div class="col-sm-12">
+              <h6 style="font-size: 20px; color: blue;"><img src="image/inves.jpg" class="img-fluid" style="height: 25px; width: 30px; color: blue;"><a href="#Investment"><b>Investment</b></a><p style="font-size: 15px; margin-left: 30px; color: black;">Great idea to consumers</p></h6>
+            </div>
+            <div class="col-sm-12">
+              <h6 style="font-size: 20px; color: blue;"><img src="image/images.png" class="img-fluid" style="height: 25px; width: 30px; color: blue;"><a href=""><b>Consultancy</b></a><p style="font-size: 15px; margin-left: 30px; color: black;">Authentic information to research innovation</p></h6>
+            </div>
+            <div class="col-sm-12">
+              <h6 style="font-size: 20px; color: blue;"><img src="image/sus.png" class="img-fluid" style="height: 25px; width: 30px; color: blue;"><a href="#Freedom"><b>Freedom Venture</b></a><p style="font-size: 15px; margin-left: 30px; color: black;">Service oppurtunity to investors profitability</p></h6>
+            </div>
+            <div class="col-sm-12">
+              <h6 style="font-size: 20px; color: blue;"><img src="image/freedom.png" class="img-fluid" style="height: 25px; width: 30px; color: blue;"><a href="#Funding"><b>Funding</b></a><p style="font-size: 15px; margin-left: 30px; color: black;">Bring ideas to life</p></h6>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+   </div>
+
+ </div>
+ </section><br><br>
+
+ <div class="container-fluid box" id="Financial">
+    <img src="image/financial.png" class="img img-fluid mobiler">
+      <div class="row">
+        <div class="col-sm-12 text">
+      <div class="">
+        <h1 align="left" style="font-size:2.5vw;"><b>Financial Service</b></h1>
+        <h5 align="left" style="color: black; font-size:1.5vw;">Assuring financial goals for you by reducing financial complexity. Great guaranteed return along with expected return to fulfill your personal happiness and, by reducing saving risk.</h5>
+        <button class="btn-change9 btn-responsive btn-sm pl-1 pr-1 mt-0" style=" font-size:0.9vw;">| Explore ></button>
+      </div>
+      </div>
+      </div>
+  </div><br><br>
+
+  <div class="container-fluid box1 mt-5" id="Investment">
+    <img src="image/young.jpg" class="img img-fluid inverstmentman">
+      <div class="text1 mt-0">
+        <h1 align="left"><b style="color: black; font-size:2vw;">Investment Management</b></h1>
+        <h6 align="left" style="color: black; font-size:1.3vw;">Investment service is designed to reach your organisation to next level of consumer eccentric brand. Strategy innovation and deep analysis for your business.</h6>
+        <button class="btn-change4 btn-responsive btn-sm pl-1 pr-1" style="color: red; font-size:0.9vw;">| Explore ></button>
+      </div>
+  </div><br><br>
+
+  <div class="container-fluid box2 mt-5" id="Freedom">
+    <img src="image/finan.jpg" class="img img-fluid freedomimg">
+      <div class="text2" align="left">
+        <h1 align="left"><b style="color: #BF2105; font-size:2.4vw;">Freedom Venture</b></h1>
+        <h6 align="left" style="color: black; font-size:1.3vw;">Nurture small businesses by encouraging the community inclusion with convenience investment. Amplifying the profitability of local and small investors who are passionate about product value.</h6>
+        <center><button class="btn-change5 btn p-1 pb-1 " style="font-size:1vw;">| Supplier ></button></center>
+        <center><button class="btn-change5 btn p-1 pb-1 ml-2" style="font-size:1vw;">|| Investor >></button></center>
+      </div>
+  </div><br><br>
+
+  <div class="container-fluid box3 mt-5" id="Funding">
+    <img src="image/home.jpg" class="img img-fluid Fundingimg">
+      <div class="text3">
+        <h1 align="left" style="font-size:2.5vw;"><b>Funding</b></h1>
+        <p align="left" style="color: black; font-size:1.3vw;">For those who actualise the imagination into reality. Working side by side with enthusiastic people, to provide financial, technical, human & research support to develop world-class companies. Discovering the problem solving ideas across globe.</p>
+      </div>
+  </div><br><br>
+
+</div><!--end Container-fluid-->
+</section>
+
+<?php include "link/footer.html" ?>
+
+          
+          
+          
---
+++ investment.php
@@ -0,0 +1,121 @@
+ <!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Investment Management</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<section id="Investment-Management">
+	  
+	  <div class="p-2 bg-dark">
+	    <h6 align="center" style="color: white;"><b style="color: red;">Save NOW</b> for the most secure, unique and profitable savings with <a href="" style="color: white">0% risk, and assured return upto 23%</a> & beyond</h6>
+	  </div>
+	  
+	  <div class="container row">
+	    <div class="col-sm-6">
+	      <a class="navbar-brand" href="index.php">
+	        <img src="image/logo2.png" class="img-fluid" alt="logo" style="padding-right: 0px; padding-left: 0px; padding-top: 0px; padding-bottom: 0px; width: 300px;">
+	      </a>
+	    </div>
+	    <div class="col-sm-5"></div>
+	    <div class="col-sm-1" align="right">
+	      <button class="btn-change pl-2 pr-2">Open Account<i class="fa fa-plus ml-2"></i></button>
+	    </div>
+	  </div>
+
+	  <div class="container row">
+	    <div class="col-sm-1"></div>
+	    <div class="col-sm-5">
+	      <div class="mt-5">
+	        <h1 align="left" style="color: black;"><b>Because You have earned it,</b></h1>
+	        <h2 align="left" style="color: black; font-size: 50px;"><b>You ask for it, and we serve you</b></h2>
+	        <div class="btn-group btn-group-sm">
+	          <button class="btn-change7 pl-1 pr-1">Start Saving now</button>
+	          <button class="btn-change6 pl-1 pr-1">Live value 27* %</button>
+	        </div>
+	        <p align="center">Get best return from your saving</p>
+	      </div>
+	    </div>
+	    <div class="col-sm-6">
+	      <img src="image/hhh.png" class="img img-fluid ml-2" style="height: 100%; width: 100%;">
+	    </div>
+	  </div><br><br>
+
+	  <div class="container row">
+	  	<div class="col-sm-1"></div>
+	    <div class="col-sm-5">
+	      <div class="mt-5 ml-5">
+	      	<h4 style="color: black;"><b style="color: red;">0</b>. <b style="color: red;">Because you deserve best</b></h4>
+	      	<h5 style="color: blue; margin-left: 25px;">Expert advisory and advance research to nurture your savings</h5>
+	      	
+	      	<h4 style="color: black; margin-top: 30px;"><b>1</b>. <b>Experience</b></h4>
+	      	<h5 style="color: black; margin-left: 25px;">We believe it should be real, worth and valuable</h5>
+
+	      	<h4 style="color: black; margin-top: 30px;"><b>2</b>. <b>Our promise</b></h4>
+	      	<h5 style="color: black; margin-left: 25px;">Higher profit for You along with 8-23% guaranteed return for you. <b>Profitable ever</b></h5>
+
+	      	<h4 style="color: black; margin-top: 30px;"><b>3</b>. <b>Dedication</b></h4>
+	      	<h5 style="color: black; margin-left: 25px;">To serve you; something you need, the way it should be and the way you want</h5>
+
+	      </div>
+	    </div>
+	    <div class="col-sm-6 bg-light">
+	      <div class="mt-4">
+	      	<h5 style="color: red;"><b>Higher profit on your personal saving investment.</b></h5>
+	      	<h5 align="center">Submit the form below</h5>
+	      	<h6 class="ml-2 mt-5 mb-5" style="color: blue;"><b>Let's make your financial planning easy, happier & profitable</b></h6>
+	      </div>
+	      <form action="#">
+	      	<div class="form-inline">
+	            <div class="form-group ml-2">
+	              <input type="text"  class="form-control b-2 textcol" placeholder="Full Name" id="Name" ng-model="Name">
+	            </div>
+	            <div class="form-group ml-2">
+	              <input type="email" class="form-control textcol" placeholder="Work email*" ng-model="email" id="email">
+	            </div>
+            </div>
+            <div class="form-inline mt-3 ml-2">
+	            <div class="form-group">
+	              <input type="text"  class="form-control b-2 textcol" placeholder="Mobile*" id="phonenumbers" ng-model="phonenumbers">
+	            </div>
+	            <div class="form-group ml-2">
+	              <select name="cars" class="custom-select form-control" style="padding-left: 70px; padding-right: 70px;">
+				    <option selected>Saving Type       </option>
+				    <option value="volvo">Short-term</option>
+				    <option value="fiat">Mid-term</option>
+				    <option value="audi">Long-term</option>
+				    <option value="fiat">Student</option>
+				    <option value="audi">Customisable</option>
+				  </select>
+	            </div>
+            </div>
+            
+            <div class="form-group mt-3 ml-2 mr-4">
+              <textarea class="form-control textcol" placeholder="What is your purpose of saving?" rows="3" id="purpose" ng-model="purpose"></textarea>
+            </div>
+
+            <div class="form-inline">
+	            <div class="form-group ml-2">
+	              <input type="text"  class="form-control b-2 textcol" placeholder="Salary/income yearly" id="Name" ng-model="Name">
+	            </div>
+	            <div class="form-group ml-2">
+	              <input type="email" class="form-control textcol" placeholder="Salaried/Self-emploied/Student" ng-model="email" id="email">
+	            </div>
+            </div>
+            
+            <button type="submit" class="btn btn-primary ml-2 mt-3">Submit</button>
+          </form><br><br>
+	    </div>
+	  </div><hr><br><br>
+
+
+	</section>
+</body>
+</html>
